328118aa4df97b12e3163966cd6c7692fa5b9d32
Nicolas Seinlet <nse@odoo.com>
2015-06-03 16:09:20 +0200

    Training module

---
+++ openacademy/__openerp__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "openacademy",
+
+    'summary': """
+        Manage trainings""",
+
+    'description': """
+        Open Academy module for managing trainings:
+            - training courses
+            - training sessions
+            - attendees registration
+    """,
+
+    'author': "Your Company",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Test',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'board', 'website'],
+
+    # always loaded
+    'data': [
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        'templates.xml',
+        'views/openacademy.xml',
+        'views/partner.xml',
+        'views/session_workflow.xml',
+        'views/session_board.xml',
+        'reports.xml',
+        'templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo.xml',
+    ],
+}
---
+++ openacademy/controllers.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from openerp import http
+
+class Academy(http.Controller):
+    @http.route('/academy/academy/', auth='public', website=True)
+    def index(self, **kw):
+        Teachers = http.request.env['res.partner']
+        return http.request.render('openacademy.index', {
+            'teachers': Teachers.search([('instructor', '=', True)])
+        })
+        
+    @http.route('/academy/<model("res.partner"):teacher>/', auth='public', website=True)
+    def teacher(self, teacher):
+        return http.request.render('openacademy.biography', {
+            'person': teacher
+        })
+        
