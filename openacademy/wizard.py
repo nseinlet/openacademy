1c40764a428314474cea37653997a66697484654
Nicolas Seinlet <nse@odoo.com>
2023-10-24 11:52:43 +0200

    Fix the typo

--- openacademy/wizard.py
+++ openacademy/wizard.py
@@ -12,7 +12,6 @@ class Wizard(models.TransientModel):
         string="Sessions", required=True, default=_default_sessions)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
     
-    @api.multi
     def subscribe(self):
         for session in self.session_ids:
             session.attendee_ids |= self.attendee_ids
